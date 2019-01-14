0dfcb6992515b91a7b0f54c6aef9b9f0209d4880
MARS-HOME-PC\Mars <flyingmars@gmail.com>
2019-01-15 00:45:05 +0800

    完成漲勢回測

---
+++ js/script.js
@@ -0,0 +1,37 @@
+function updateBullRetuenTable(){
+    var bullLow  = parseFloat( $( "#bull-return-low" ).val()) ;
+    var bullHigh = parseFloat( $( "#bull-return-high" ).val()) ;
+    var diff = bullHigh - bullLow ;
+    var ratio = [191,236,333,382,500,618,666,764,809,875];
+    for ( var i = 0 ; i < ratio.length ; i++){
+        $( "#bull-return-0" + ratio[i] ).html( Math.round( (bullHigh - diff * ratio[i] / 1000 ) * 100) / 100 );
+    }
+}
+
+$(document).ready(function(){
+    $( "#bull-return-low" ).keyup(function() {
+        updateBullRetuenTable();
+    });
+    $( "#bull-return-high" ).keyup(function() {
+        updateBullRetuenTable();
+    });
+    $("#bull-return-low-left").click(function(){
+        $( "#bull-return-low" ).val( parseInt( $( "#bull-return-low" ).val() ) - 1 ) ;
+        updateBullRetuenTable();
+    });
+    $("#bull-return-low-right").click(function(){
+        $( "#bull-return-low" ).val( parseInt( $( "#bull-return-low" ).val() ) + 1 ) ;
+        updateBullRetuenTable();
+    });
+    $("#bull-return-high-left").click(function(){
+        $( "#bull-return-high" ).val( parseInt( $( "#bull-return-high" ).val() ) - 1 ) ;
+        updateBullRetuenTable();
+    });
+    $("#bull-return-high-right").click(function(){
+        $( "#bull-return-high" ).val( parseInt( $( "#bull-return-high" ).val() ) + 1 ) ;
+        updateBullRetuenTable();
+    });    
+    $( "#bull-return-high" ).val(9600);
+    $( "#bull-return-low" ).val(9500);
+    updateBullRetuenTable();
+});
