aa4625d9ce1ef46c741d5db88cfdb0e0052add80
MARS-HOME-PC\Mars <flyingmars@gmail.com>
2019-01-15 01:01:14 +0800

    加入GOOGLE ANALYTICS

--- js/script.js
+++ js/script.js
@@ -35,3 +35,13 @@ $(document).ready(function(){
     $( "#bull-return-low" ).val(9500);
     updateBullRetuenTable();
 });
+
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-114930036-2"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-114930036-2');
+</script>
