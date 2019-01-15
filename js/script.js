cc13b0e2c3a4e02434f293c17d89cb93a31edcc9
mars <flyingmars@gmail.com>
2019-01-15 09:37:16 +0800

    修正 GOOGLE ANALYTICS

--- js/script.js
+++ js/script.js
@@ -36,12 +36,3 @@ $(document).ready(function(){
     updateBullRetuenTable();
 });
 
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-114930036-2"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'UA-114930036-2');
-</script>
