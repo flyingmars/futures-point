40a11ab626e6bef4d831e4e93ae19a50bf90f001
MARS-HOME-PC\Mars <flyingmars@gmail.com>
2019-01-20 21:51:59 +0800

    add H properties

--- js/script.js
+++ js/script.js
@@ -3,10 +3,19 @@ var pageValueRecord = {} ;
 function updateBullRetuenTable(){
     var bullLow  = parseFloat( $( "#bull-return-low" ).val()) ;
     var bullHigh = parseFloat( $( "#bull-return-high" ).val()) ;
+    var bullBack = parseFloat( $( "#bull-return-back" ).val()) ;
     var diff = bullHigh - bullLow ;
-    var ratio = [191,236,333,382,500,618,666,764,809,875];
+    var ratio = [191,236,333,382,500,618,666,764,809,875,1000,1100,1191,1382,1500,1618,1809,2000,3000];
     for ( var i = 0 ; i < ratio.length ; i++){
-        $( "#bull-return-0" + ratio[i] ).html( Math.round( (bullHigh - diff * ratio[i] / 1000 ) * 100) / 100 );
+        if ( $("#h-switch").prop("checked") ){
+            if ( $("#drawer-list a.active").attr('id') == "bull-return-menu" ){
+                $( "#bull-return-" + ratio[i] ).html( Math.round( (bullBack + diff * ratio[i] / 1000 ) * 100) / 100 );
+            }else{
+                $( "#bull-return-" + ratio[i] ).html( Math.round( (bullBack - diff * ratio[i] / 1000 ) * 100) / 100 );
+            }
+        }else{
+            $( "#bull-return-" + ratio[i] ).html( Math.round( (bullHigh - diff * ratio[i] / 1000 ) * 100) / 100 );
+        }
     }
 }
 
@@ -21,7 +30,7 @@ function recoverRecordNumber(){
 }
 
 function changePageTo(destination){
-    var toRecord = [$('.record-1').val(),$('.record-2').val(),$('.record-3').val()];
+    var toRecord = [$('.record-1').val(),$('.record-2').val(),$('.record-3').val(),$("#h-switch").prop("checked")];
     recordNumberBeforeChangePage(toRecord);
     
     $("#drawer-list a.active").removeClass('active');
@@ -40,6 +49,17 @@ function renderP1(){
         $('.p1.bear').show();
         $('.p1.bull').hide();        
     }
+    
+    // Bind all listener
+    $( "#bull-return-low" ).unbind("keyup");
+    $( "#bull-return-high" ).unbind("keyup");
+    $("#bull-return-low-left").unbind("click");
+    $("#bull-return-low-right").unbind("click");
+    $("#bull-return-high-left").unbind("click");
+    $("#bull-return-high-right").unbind("click");
+    $("#bull-return-back-left").unbind("click");
+    $("#bull-return-back-right").unbind("click");
+    $("#h-switch").unbind("change");
     $( "#bull-return-low" ).keyup(function() {
         updateBullRetuenTable();
     });
@@ -61,16 +81,47 @@ function renderP1(){
     $("#bull-return-high-right").click(function(){
         $( "#bull-return-high" ).val( parseInt( $( "#bull-return-high" ).val() ) + 1 ) ;
         updateBullRetuenTable();
-    });    
+    });
+    $("#bull-return-back-left").click(function(){
+        $( "#bull-return-back" ).val( parseInt( $( "#bull-return-back" ).val() ) - 1 ) ;
+        updateBullRetuenTable();
+    });
+    $("#bull-return-back-right").click(function(){
+        $( "#bull-return-back" ).val( parseInt( $( "#bull-return-back" ).val() ) + 1 ) ;
+        updateBullRetuenTable();
+    });
     
+    $("#h-switch").change(function(){
+        renderSwitch();
+    });
+    
+    // recover all numbers
     var recoverdNumber = recoverRecordNumber() ;
     if (recoverdNumber === undefined){
         $( "#bull-return-high" ).val(9600);
         $( "#bull-return-low" ).val(9500);
+        $( "#bull-return-back" ).val(9550);
     }else{
         $( "#bull-return-high" ).val(recoverdNumber[0]);
         $( "#bull-return-low" ).val(recoverdNumber[1]);
+        $( "#bull-return-back" ).val(recoverdNumber[2]);
     }
+    
+    renderSwitch();    
+    updateBullRetuenTable();
+    
+}
+
+
+function renderSwitch(){
+    if ( $("#h-switch").prop("checked") ){
+        $(".h-only").show();
+        $(".return-only").hide();
+    }else{
+        $(".h-only").hide();
+        $(".return-only").show();
+    }
+    
     updateBullRetuenTable();
     
 }
@@ -78,6 +129,7 @@ function renderP1(){
 $(document).ready(function(){
     $('#drawer-list a').click(function(){
         changePageTo($(this).attr('id'));
+        $('.bmd-layout-backdrop').click();
     });
     
     renderP1("bull");
